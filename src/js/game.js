8987976b46b91de449780655a03b1f54414a657b
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-11-02 20:02:34 +0300

    Update game.js

--- src/js/game.js
+++ src/js/game.js
@@ -28,8 +28,8 @@ export default class Game {
       ];
       //активная фигура
       this.activePiece = {
-         x: 0,
-         y: 0,
+         x: 0,//текущая позиция фигуры по горизонтали
+         y: 0,//текущая позиция фигуры по веертикали
          blocks: [
             [0, 1, 0],
             [1, 1, 1],
@@ -39,24 +39,24 @@ export default class Game {
    }
    //перемещение
    movePieceLeft() {
-      this.activePiece.x -= 1;
-      if (this.isPieceOutOfBounds()) {
-         this.activePiece.x += 1;
-         this.lockPiece();
+      this.activePiece.x -= 1;//пермещение фигуры влево
+      if (this.hasCollusion()) {//если фигура сталкивается с границей или другой фигурой
+         this.activePiece.x += 1;//возвращаем фигуру на предыдущую позицию
+         this.lockPiece();//фиксируем фигуру на игровом поле
       }
    }
    movePieceRight() {
-      this.activePiece.x += 1;
-      if (this.isPieceOutOfBounds()) {
-         this.activePiece.x -= 1;
-         this.lockPiece();
+      this.activePiece.x += 1;//пермещение фигуры вправо
+      if (this.hasCollusion()) {//если фигура сталкивается с границей или другой фигурой
+         this.activePiece.x -= 1;//возвращаем фигуру на предыдущую позицию
+         this.lockPiece();//фиксируем фигуру на игровом поле
       }
    }
    movePieceDown() {
-      this.activePiece.y += 1;
-      if (this.isPieceOutOfBounds()) {
-         this.activePiece.y -= 1;
-         this.lockPiece();
+      this.activePiece.y += 1;//пермещение фигуры вниз
+      if (this.hasCollusion()) {//если фигура сталкивается с границей или другой фигурой
+         this.activePiece.y -= 1;//возвращаем фигуру на предыдущую позицию
+         this.lockPiece();//фиксируем фигуру на игровом поле
       }
    }
    //метод возвращает за пределами границ мы или нет
@@ -79,10 +79,10 @@ export default class Game {
       const { y: pieceY, x: pieceX } = this.activePiece;
       const blocks = this.activePiece.blocks;
       //внешний цикл перебирает ряды, внутренний цикл перебирает элементы ряда
+      // копируем значения из матрицы фигуры в игровое поле
       for (let row = 0; row < blocks.length; row++) {
          for (let col = 0; col < blocks[row].length; col++) {
-            if (blocks[row][col]) {
-               //копируем значения из массива blocks в массив playfield
+            if (blocks[row][col]) {               
                this.playfield[pieceY + row][pieceX + col] = blocks[row][col]
             }
          }
